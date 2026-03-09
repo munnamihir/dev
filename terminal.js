070cbeb6251da39124778e5235013a3fa40c0830
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-09 13:55:54 -0400

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,40 +1,48 @@
 const input = document.getElementById("commandInput");
 const output = document.getElementById("output");
 
-function print(text) {
-  const div = document.createElement("div");
-  div.className = "output-line";
-  div.innerHTML = text;
-  output.appendChild(div);
+function print(text){
+
+const div = document.createElement("div");
+
+div.className="output-line";
+
+div.innerHTML=text;
+
+output.appendChild(div);
+
 }
 
-function runCommand(cmd) {
+function runCommand(cmd){
 
-  const command = cmd.trim().toLowerCase();
+const command = cmd.trim().toLowerCase();
 
-  if(commands[command]) {
-      print(commands[command]());
-  } else {
-      print("Command not found. Type 'help'");
-  }
+if(commands[command]){
+
+print(commands[command]());
+
 }
 
-input.addEventListener("keydown", function(e){
+else{
 
-  if(e.key === "Enter") {
+print("Command not found. Type 'help'");
 
-      const value = input.value;
+}
 
-      print(`<span class="prompt">mihir@portfolio:~$</span> ${value}`);
+}
 
-      runCommand(value);
+input.addEventListener("keydown",function(e){
 
-      input.value = "";
-  }
+if(e.key==="Enter"){
+
+const value=input.value;
+
+print(`<span class="prompt">mihir@system:~$</span> ${value}`);
+
+runCommand(value);
+
+input.value="";
+
+}
 
 });
-
-print("Booting MihirOS...");
-print("Loading QuantumMail architecture...");
-print("System Ready.");
-print("Type 'help' to begin.");
